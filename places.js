89c3caa04a58937527a654af7243636b607e7058
xavier-arcamo <xavier.arcamo@gmail.com>
2026-03-01 18:02:44 -0500

    Add files via upload

---
+++ places.js
@@ -0,0 +1,20 @@
+// x = pixels from LEFT
+// y = pixels from TOP
+window.PLACES = [
+  {
+    name: "Katreios",
+    x: 2100,
+    y: 1650,
+    type: "city",
+    description: "Imperial capital. Seat of the Senate. (Add lore here.)",
+    tags: ["capital", "imperial"]
+  },
+  {
+    name: "Black Runner Shrine",
+    x: 980,
+    y: 3120,
+    type: "shrine",
+    description: "Full-moon rites; locals leave offerings of iron and ash.",
+    tags: ["moon", "marchlands"]
+  }
+];
